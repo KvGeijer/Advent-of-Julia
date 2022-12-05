e342e0a65a758803a64ce37431f6154ca24d0536
Kåre von Geijer <kare.kvg@gmail.com>
2022-12-05 10:52:37 +0100

    Change to short solution
    Maybe a bit less readable, but it is fun to keep it short as well

--- day05/main-pretty.jl
+++ day05/main-pretty.jl
@@ -1,34 +1,25 @@
 include("utils.jl")
 
-function part1(instructions, stacks)
-    for instr in split.(instructions)
-        (nbr, from, to) = parse.(Int, (instr[2], instr[4], instr[6]))
-
-        append!(stacks[to], [pop!(stacks[from]) for _ in 1:nbr])
-    end
-    stacks
-end
-
-function part2(instructions, stacks)
-    for instr in split.(instructions)
-        (nbr, from, to) = parse.(Int, (instr[2], instr[4], instr[6]))
-        
-        append!(stacks[to], reverse([pop!(stacks[from]) for _ in 1:nbr]))
-    end
-    stacks
-end
+printstacks(stacks) =
+    println((stack[end] for stack in stacks) |> join)
 
 function main(input="input.txt")
     (pos0, instructions) = splitit(readlines(input), "")    
 
     nbrstacks = parse(Int, pos0[end][end-1])
-    stacks = [[row[col] for row in reverse(pos0[1:end-1]) if row[col] != ' ']
-                        for col in 2:4:(nbrstacks*4 - 2)]
-
-    for part in (part1, part2)
-        finalstacks = part(instructions, copy.(stacks))
-        println((stack[end] for stack in finalstacks) |> join)
+    stacks1 = [[row[col] for row in reverse(pos0[1:end-1]) if row[col] != ' ']
+                         for col in 2:4:(nbrstacks*4 - 2)]
+    stacks2 = copy.(stacks1)
+    
+    for instr in split.(instructions)
+        (nbr, from, to) = parse.(Int, (instr[2], instr[4], instr[6]))
+        
+        append!(stacks1[to],         [pop!(stacks1[from]) for _ in 1:nbr])
+        append!(stacks2[to], reverse([pop!(stacks2[from]) for _ in 1:nbr]))
     end
+
+    printstacks(stacks1)
+    printstacks(stacks2)
 end
 
 if abspath(PROGRAM_FILE) == @__FILE__
