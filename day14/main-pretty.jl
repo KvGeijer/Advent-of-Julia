9a6ad3e3ef2f4002db6eeec629c84c18fd112ee2
Kåre von Geijer <kare.kvg@gmail.com>
2022-12-14 07:30:23 +0100

    Add more compact version of day 14
    Don't actually know how much prettier it is.

---
+++ day14/main-pretty.jl
@@ -0,0 +1,46 @@
+include("utils.jl")
+
+function parse_rocks(input)
+    rocks = Set()
+    for line in readlines(input)
+        coords = map(pair -> parse.(Int, pair), split.(split(line, " -> "), ','))
+        for (prev, next) in zip(coords[1:end-1], coords[2:end])
+            diff = to_base_vecs(next - prev)
+            while (prev != next)
+                push!(rocks, prev)
+                prev = prev + diff
+            end
+            push!(rocks, prev)
+        end
+    end
+    rocks
+end
+
+function main(input="input.txt")
+    debris = parse_rocks(input)
+
+    bottom = maxby(p -> p[2], debris)[2]
+
+    part1 = sandunits = 0
+    while !([500, 0] in debris)
+        sandpos = [500, 0]
+        
+        while true
+            dirs = [[dx, 1] for dx in [0,-1,1] if !(sandpos + [dx,1] in debris)]
+            if isempty(dirs) || sandpos[2] == (bottom + 1)
+                push!(debris, sandpos)
+                break
+            else
+                sandpos += first(dirs)
+            end                
+        end
+        part1 = sandpos[2] > bottom && part1 == 0 ? sandunits : part1
+        sandunits += 1
+    end
+    println(part1)
+    println(sandunits)
+end
+
+if abspath(PROGRAM_FILE) == @__FILE__
+    main()
+end
